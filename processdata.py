ca14e0f5699d0ce6e66c820135836bb4763e9e6a
954880786 <954880786@qq.com>
2017-01-09 19:51:34 +0800

    Add files via upload

--- processdata.py
+++ processdata.py
@@ -1,14 +1,63 @@
 #encoding=utf-8
-from  DataProcess import clac_word_freq,clac_news_hot,classify_news,rm_samenews,hot_muti_count
+import time
 
+from  DataProcess import clac_word_freq,clac_news_hot,classify_news,rm_samenews,hot_muti_count
+from PrepareJson import gen_json
+st=time.time()
+
+#3s
 a = clac_word_freq.CalcFreq()
 a.run()
 
+print time.time()-st
+st=time.time()
+
+#5s
 b=clac_news_hot.CalcNewsHot()
 b.run()
 
+print time.time()-st
+st=time.time()
+
+#99s
 c=classify_news.newsClassier()
 c.run()
 
-d.
+print time.time()-st
+st=time.time()
 
+#75s
+d=rm_samenews.Deduplication()
+d.run()
+
+print time.time()-st
+st=time.time()
+
+#5s
+e=hot_muti_count.CalcNewsHot()
+e.run()
+
+#--------------------Prepare JSon-------------------------------
+
+##print time.time()-st
+##st=time.time()
+##
+###85s
+##f=gen_json.genJsons()
+##f.prepare_words()
+##
+##
+##print time.time()-st
+##st=time.time()
+##
+###4s
+##f.prepare_news()
+##
+##print time.time()-st
+##st=time.time()
+##
+###1s
+##f.prepare_topics()
+##
+##print time.time()-st
+##st=time.time()
